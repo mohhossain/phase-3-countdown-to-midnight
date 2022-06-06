3c1c2426b19c3eb2cd3de709ba0eb4fa0bb115a4
Mohammad <moh.hossain98@gmail.com>
2022-06-06 11:17:41 -0400

    commit tree 6aae3df2dacbb6049985828bc476b1570340ac60

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+# def countdown(seconds_to_midnight)
+#     while seconds_to_midnight > 0
+#       puts "#{seconds_to_midnight} SECOND(S)!"
+#       seconds_to_midnight -= 1
+#     end
+#     "HAPPY NEW YEAR!"
+#   end
+
+def countdown_with_sleep(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
